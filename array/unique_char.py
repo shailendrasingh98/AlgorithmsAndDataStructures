3af9aa2976d3bd4547efb51a57cf8449a57fef8f
Shailendra Singh <shailendrasingh@Shailendras-MacBook-Air.local>
2025-03-29 21:10:27 +0530

    Adding Blind 75 Leetcode problems with solutions

--- array/unique_char.py
+++ array/unique_char.py
@@ -26,9 +26,16 @@ def isuniqueChar2(string):
 def isuniqueChar3(string):
     checker = 0
     for char in string:
+        # ord(char) returns the ASCII value of the character, and ord('a') returns the ASCII value of 'a'. 
+        # The difference between the two gives a number between 0 and 25, corresponding to the position of 
+        # the letter in the alphabet ('a' = 0, 'b' = 1, ..., 'z' = 25). This value is stored in val.
         val = ord(char) - ord('a')
-        if ((checker & (1<<val))>0): #16 & 1<<4 = 0
+
+        #1 << val shifts the number 1 to the left by val positions. This creates a number where only the val-th bit is set.
+        # checker & (1 << val) checks whether the val-th bit in checker is already set
+        if ((checker & (1<<val))>0): #16 & 1<<4 = 1
             return False
+        #This line updates checker by setting the val-th bit to 1. 
         checker|=1<<val # 0 | 1<<4 = 16
     return True
 
